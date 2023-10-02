1622b3028dcea2f491ca6f87c921c01713b35444
a24985041a <a24985041a@gmail.com>
2023-10-02 19:30:36 +0800

    modify app.py allow user input number

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
-# 猜數字
-import random
+def addnum(a,b):
+    return a+b
 
-answerNum = random.randint(1, 100)
-for i in range(1, 8):
-    guess = input("請在7次內猜出數字：")
-    if guess == "" or not guess.isdigit():
-        break
-    elif int(guess) == answerNum:
-        print("讚！答對了^_^ ")
-        quit()
-    elif int(guess) > answerNum:
-        print("> 猜低些試試")
-    elif int(guess) < answerNum:
-        print("> 猜高些試試")
-print("\n正確答案是：" + str(answerNum))
-input()
+x = int(input("Please input number1："))
+y = int(input("Please input number2："))
+print(addnum(x,y))
